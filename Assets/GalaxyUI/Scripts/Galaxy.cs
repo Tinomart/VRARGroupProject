377ab2757f7414f037dca9bc07d8122a4652f115
Tino Marte <tinminator02@gmail.com>
2025-12-03 20:52:23 +0100

    Added Basic Galaxy Interaction

---
+++ Assets/GalaxyUI/Scripts/Galaxy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+/// <summary>
+/// Locks rotation on specified axes for both single and two-handed grabs
+/// Add this to your XR Grab Interactable GameObject
+/// </summary>
+
+public class Galaxy : MonoBehaviour
+{
+    
+}
