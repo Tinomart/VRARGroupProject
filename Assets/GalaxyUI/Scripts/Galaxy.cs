97a5458e938e456197a0b2eb1995405e4d4ad55c
Marvin <marvin.kissling@gmx.de>
2025-12-04 22:06:19 +0100

    generator Added

--- Assets/GalaxyUI/Scripts/Galaxy.cs
+++ Assets/GalaxyUI/Scripts/Galaxy.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using System.Collections.Generic;
@@ -7,11 +8,21 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 /// Locks rotation on specified axes for both single and two-handed grabs
 /// Add this to your XR Grab Interactable GameObject
 /// </summary>
-///
-public int numOfArms = 3;
 
 public class Galaxy : MonoBehaviour
 {
-    public GameObject ArmPrefeb;
-    Transform.roattion 
+    public int numOfArms = 15;
+    public GameObject armPrefeb;
+    public List<GameObject> arms = new List<GameObject>();
+
+    private void Start()
+    {
+        for (int i = 0; i < numOfArms; i++)
+        {
+            GameObject obj = Instantiate(armPrefeb);
+            arms.Add(obj);
+            arms[i].transform.Translate(0,0,0);
+            arms[i].transform.Rotate(0, 0, (360/numOfArms)*i);
+        }   
+    }
 }
