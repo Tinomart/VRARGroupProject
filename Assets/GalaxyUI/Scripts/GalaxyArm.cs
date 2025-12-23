d9a62457db374b692f2290ebc0f462966c405bdd
Tino Marte <tinminator02@gmail.com>
2025-12-23 21:44:10 +0100

    Added Visual Feedback for which stars teleport you where and what arm is selected. Also added Galaxy arm removal of stars and automatic star arrangement. Also removed clamped scale and added functionality of disappearing galaxy at small scales.

--- Assets/GalaxyUI/Scripts/GalaxyArm.cs
+++ Assets/GalaxyUI/Scripts/GalaxyArm.cs
@@ -1,23 +1,33 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class GalaxyArm : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private List<GameObject> stars = new List<GameObject>();
+    
+    
     private List<Transform> starsTransforms = new List<Transform>();
     public float spiralRadius = 12f;
     public float spiralAngle = 2f;
-
-    public float quadraticA = 1f;
+    
     public Vector3 offset = new Vector3(1,0,0);
-    public float quadraticB = 0f;
-    public float quadraticC = 0f;
+    
     public int starCount = 12;
+    private int starHoverCount = 0;
     
     public GameObject starPrefab;
+    public GameObject label;
+    [HideInInspector]
+    public TextMeshProUGUI labelText;
+    public String armName = "None";
+    
+    private List<XRDirectInteractor> _interactors = new List<XRDirectInteractor>();
+    
         
     public void Setup()
     {
@@ -26,8 +36,70 @@ public class GalaxyArm : MonoBehaviour
             GameObject obj = Instantiate(starPrefab);
             obj.transform.SetParent(transform);
             stars.Add(obj);
+            obj.GetComponent<Star>().galaxyArm = this;
         }
         
+        labelText = label.GetComponentInChildren<TextMeshProUGUI>();
+        labelText.text = armName;
+        
+        RearrangeStars();
+        
+        
+    }
+    
+    Vector3 CalculateCurvePosition(int starNumber)
+    {
+        float posX = (float)Math.Cos(starNumber*spiralAngle/stars.Count) * starNumber*spiralRadius/stars.Count;
+        float posY = (float)Math.Sin(starNumber*spiralAngle/stars.Count) * starNumber*spiralRadius/stars.Count;
+        return new Vector3(posX, 0, posY) + offset;
+    }
+
+    public void StarHoverEnter()
+    {
+        starHoverCount++;
+        label.SetActive(true);
+    }
+    public void StarHoverExit()
+    {
+        starHoverCount--;
+        if (starHoverCount == 0)
+        {
+            label.SetActive(false);
+        }
+        
+    }
+
+    public void RemoveStar(GameObject star)
+    {
+        if (star.GetComponent<Star>())
+        {
+            stars.Remove(star);
+            RearrangeStars();
+        }
+        else
+        {
+            Debug.LogError("The object that was tried to be removed is not a star.");
+        }
+    }
+    
+    public void AddStar(GameObject star)
+    {
+        if (star.GetComponent<Star>())
+        {
+            stars.Add(star);
+            RearrangeStars();
+            star.GetComponent<Star>().galaxyArm = this;
+        }
+        else
+        {
+            Debug.LogError("The object that was tried to be added is not a star.");
+        }
+        
+    }
+
+    private void RearrangeStars()
+    {
+        starsTransforms = new List<Transform>();
         foreach (GameObject star in stars)
         {
             starsTransforms.Add(star.transform);
@@ -41,21 +113,10 @@ public class GalaxyArm : MonoBehaviour
 
         }
         
+        label.transform.localPosition = CalculateCurvePosition(starcount);
     }
-    
-    Vector3 CalculateCurvePosition(int starNumber)
-    {
-        float posX = (float)Math.Cos(starNumber*spiralAngle/stars.Count) * starNumber*spiralRadius/stars.Count;
-        float posY = (float)Math.Sin(starNumber*spiralAngle/stars.Count) * starNumber*spiralRadius/stars.Count;
-        return new Vector3(posX, 0, posY) + offset;
-    }
-    
-    float Quadratic(float x)
-    {
-        return quadraticA * (float)Math.Pow(x, 2) + quadraticB * x + quadraticC;
-    }
-    
-   
+
+
     // Update is called once per frame
     void Update()
     {
